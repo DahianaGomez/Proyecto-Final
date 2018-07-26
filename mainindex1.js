7271eb01fe3ee9b9640d3264a7f283bb83a1a54f
paulinaALV <39799833+paulinaALV@users.noreply.github.com>
2018-07-25 20:12:41 -0500

    rayito

--- mainindex1.js
+++ mainindex1.js
@@ -1,37 +1,37 @@
 
-   // Initialize Firebase
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBNc2Ye2xjMU5QYRztEM8dJcgUlhWtWdNc",
     authDomain: "foodplace-c4798.firebaseapp.com",
     databaseURL: "https://foodplace-c4798.firebaseio.com",
     projectId: "foodplace-c4798",
-    storageBucket: "foodplace-c4798.appspot.com",
+    storageBucket: "",
     messagingSenderId: "510515919484"
   };
   firebase.initializeApp(config);
 
-  var data = {};
-  data.userName = 'Dahiana';
+  jQuery(document).ready(function($) {
+
+  });
+
+  const userName = ();
   const database = firebase.database();
 
   $('button').click( function( event ) {
   event.preventDefault();
-  console.log('data', data);
-  data.mensaje = $('#mensaje').val();
-  ponerMensaje(data);
+  var mensaje = $('#mensaje').val();
 
-  //var data = { usuario: userName, mensaje: mensaje };
-  /*database.ref('chat/').push(data, function(err) {
+  var data = { usuario: userName, mensaje: mensaje };
+  database.ref('chat/').push(data, function(err) {
     if (err) { throw err;}
     else { 
-      console.info('Guardamos la información');
+      console.info('Guardamos la informacion');
+      ponerMensaje(data);
       $('#mensaje').val('');
     }
-  });*/
+  });
+
 });
 
 function ponerMensaje (dahia){
-  console.log('data', dahia);
-  $('.caja').append(dahia.userName + ": " + dahia.mensaje);
-}
-
+  $('#caja').append('<p>' + dahia.usuario + ': ' + dahia.mensaje +  '<p>');

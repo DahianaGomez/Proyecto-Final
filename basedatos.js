51f6834a5c1ce19f9c809949e79bf561fcd41a5e
paulinaALV <39799833+paulinaALV@users.noreply.github.com>
2018-07-27 20:03:37 -0500

    rayito

---
+++ basedatos.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyBNc2Ye2xjMU5QYRztEM8dJcgUlhWtWdNc",
+    authDomain: "foodplace-c4798.firebaseapp.com",
+    databaseURL: "https://foodplace-c4798.firebaseio.com",
+    projectId: "foodplace-c4798",
+    storageBucket: "foodplace-c4798.appspot.com",
+    messagingSenderId: "510515919484"
+  };
+  firebase.initializeApp(config);
+
+ const database = firebase.database();
+ var lugares = $ ('.container').val();
+ var datos={
+ 	reseña:$('#resena'),
+ }
